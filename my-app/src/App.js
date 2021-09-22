d785d14963d42d452562cea1c2127ef38394343b
Aiden <47725379+Mewrin@users.noreply.github.com>
2021-09-22 02:03:42 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   // Could change h1 to a navbar component and use a react router
   return (
     <div>
-      <h1 className="App"> Welcome to a basic ecommerce app :) </h1> 
+      <h1 className="App"> Welcome to a basic ecommerce app, made using React and Redux :)</h1> 
       <ECommerceComponent />
     </div>
   );
