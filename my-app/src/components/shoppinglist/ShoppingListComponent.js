7d057368ee16644669eb230b1e69e32cce7e5355
Aiden <47725379+Mewrin@users.noreply.github.com>
2021-09-25 19:03:04 +0100

    convert edge case comparison to check type too '!=' -> '!=='

--- my-app/src/components/shoppinglist/ShoppingListComponent.js
+++ my-app/src/components/shoppinglist/ShoppingListComponent.js
@@ -5,7 +5,7 @@ import { fetchShoppingList } from "./ShoppingListApi";
 import { useState } from "react";
 
 function shoppingItemsAreValid(shoppingItems) {
-    return shoppingItems != null && shoppingItems.data != undefined && shoppingItems.data.length > 0;
+    return shoppingItems !== null && shoppingItems.data !== undefined && shoppingItems.data.length > 0;
 }
 
 function ShoppingListComponent() {
@@ -13,18 +13,18 @@ function ShoppingListComponent() {
     const [shoppingItems, setShoppingItems] = useState(null);
 
     useEffect(() => {
-        const newItems = fetchShoppingList();
-        setShoppingItems(newItems);
-    });
+        setShoppingItems(fetchShoppingList());
+    }, []);
     
     return (
         <ul className="ShoppingList">
             { 
-                shoppingItemsAreValid(shoppingItems) ? (
+                shoppingItemsAreValid(shoppingItems) ? 
+                (
                     shoppingItems.data.map((item, i) => (
                         <ShoppingListItem key={item + i} itemName={item}></ShoppingListItem>
                     ))
-                )
+                ) 
                 : <p> No items currently available </p>
             }
         </ul>
